12310293621ec5487637989e571b513c62f7551c
Baivab Sarkar <109382325+ThisIs-Developer@users.noreply.github.com>
2024-01-10 02:14:37 +0530

    Update Mut_Variable.rs

--- code/Mut_Variable.rs
+++ code/Mut_Variable.rs
@@ -5,7 +5,7 @@ fn main()
     // x = 6; // error: mismatched types //x is immutable
     // println!("The value of x is: {}", x);
 
-    //lets fix it
+    //lets fix it it by using mut
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
